8315a7abb3f9870aad92d97615eae19881ab6640
David L. Day <1132144+davidlday@users.noreply.github.com>
2021-08-25 22:17:07 -0400

    Maintenance 2021 08 (#10)
    * added lint and check_build
    
    * use build and pypa publish action
    
    * test publish v* tags
    
    * added basic pyproject.toml
    
    * added standard . configs
    
    * moved code to src
    
    * converted README to markdown
    
    * moved project settings to setup.cfg
    
    * ignore line too long in code fence
    
    * formatted with black
    
    * added docstrings
    
    * removed __init__ from tests
    
    * added coding back for py 2.7
    
    * README.rst -> README.md
    
    * v1.0.1

---
+++ README.md
@@ -0,0 +1,66 @@
+# pointofview
+
+[![Latest PyPI version](https://img.shields.io/pypi/v/pointofview.svg)](https://pypi.python.org/pypi/pointofview)
+[![GitHub Workflow Status](https://github.com/prosegrinder/python-pointofview/workflows/Python%20CI/badge.svg?branch=main)](https://github.com/prosegrinder/python-pointofview/actions?query=workflow%3A%22Python+CI%22+branch%3Amain)
+
+A Python package for determining a piece of text's point of view (first, second,
+third, or unknown).
+
+## Installation
+
+`pointofview` is available on PyPI. Simply install it with `pip`:
+
+```bash
+pip install pointofview
+```
+
+You can also install it from source:
+
+```bash
+$ git clone https://github.com/prosegrinder/python-pointofview.git
+Cloning into 'python-pointofview'...
+...
+
+$ cd python-pointofview
+$ python setup.py install
+...
+```
+
+## Usage
+
+`pointofview` guesses a text's point of view by counting point of view pronouns.
+The main function `get_text_pov()` will return 'first', 'second', 'third', or
+null (Python's `None` object):
+
+```python
+>>> import pointofview
+>>> text = "I'm a piece of text written in first person! What are you?"
+>>> pointofview.get_text_pov(text)
+'first'
+```
+
+There are two other helper functions as well.
+
+`get_word_pov()` returns the point of view of a single word:
+
+```python
+>>> pointofview.get_word_pov("I")
+'first'
+>>> pointofview.get_word_pov("nope")
+None
+```
+
+`parse_pov_words()` returns a dict containing all first-, second-, and
+third-person point-of-view words:
+
+<!-- markdownlint-disable MD013 -->
+
+```python
+>>> text = """
+... When I try to analyze my own cravings, motives, actions and so forth, I surrender to a sort of retrospective imagination which feeds the analytic faculty with boundless alternatives and which causes each visualized route to fork and re-fork without end in the maddeningly complex prospect of my past.
+... """
+>>> pointofview.parse_pov_words(text)
+{'first': ['i', 'i'], 'second': [], 'third': []}
+```
+
+<!-- markdownlint-enable MD013 -->
---
+++ src/pointofview/__init__.py
@@ -0,0 +1,175 @@
+# -*- coding: utf-8 -*-
+
+"""pointofview
+
+A Python package for determining a piece of text's
+point of view (first, second, third, or unknown).
+"""
+
+import re
+from collections import OrderedDict
+
+import pkg_resources
+
+__version__ = (
+    pkg_resources.resource_string("pointofview", "VERSION").decode("utf-8").strip()
+)
+
+# Constants for use in comparisons
+FIRST = "first"
+SECOND = "second"
+THIRD = "third"
+NONE = None
+
+# NOTE:
+# Words are expected to be in lower case.
+#
+# Point of view is in order of precedence.
+# First person PoV can also contain second and third person words.
+# Second person PoV can also contain third person words.
+# Third person PoV can only contain third person words.
+POV_WORDS = OrderedDict(
+    [
+        (
+            FIRST,
+            [
+                "i",
+                "i'm",
+                "i'll",
+                "i'd",
+                "i've",
+                "me",
+                "mine",
+                "myself",
+                "we",
+                "we're",
+                "we'll",
+                "we'd",
+                "we've",
+                "us",
+                "ours",
+                "ourselves",
+            ],
+        ),
+        (
+            SECOND,
+            [
+                "you",
+                "you're",
+                "you'll",
+                "you'd",
+                "you've",
+                "your",
+                "yours",
+                "yourself",
+                "yourselves",
+            ],
+        ),
+        (
+            THIRD,
+            [
+                "he",
+                "he's",
+                "he'll",
+                "he'd",
+                "him",
+                "his",
+                "himself",
+                "she",
+                "she's",
+                "she'll",
+                "she'd",
+                "her",
+                "hers",
+                "herself",
+                "it",
+                "it's",
+                "it'll",
+                "it'd",
+                "itself",
+                "they",
+                "they're",
+                "they'll",
+                "they'd",
+                "they've",
+                "them",
+                "their",
+                "theirs",
+                "themselves",
+            ],
+        ),
+    ]
+)
+
+RE_WORDS = re.compile(r"[^\w’']+")
+
+
+def get_word_pov(word, pov_words=None):
+    """Get the point-of-view indicated by the word
+
+    Parameters:
+    ----------
+    word : str
+        The English-langauge word to find the point-of-view for
+
+    Returns:
+    -------
+    str
+        the point-of-view indicated by the word (first, second, third)
+        returns None if no point-of-view indicated
+    """
+    if pov_words is None:
+        pov_words = POV_WORDS
+    for pov in pov_words:
+        if word.lower().replace("’", "'") in (
+            pov_word.lower() for pov_word in pov_words[pov]
+        ):
+            return pov
+    return None
+
+
+def parse_pov_words(text, pov_words=None):
+    """Parse out all the point-of-view indicator words in text
+
+    Parameters:
+    ----------
+    text : str
+        a block of english languaget text
+
+    Returns:
+    -------
+    list[str]
+        a list of point-of-view indicator words
+    """
+    if pov_words is None:
+        pov_words = POV_WORDS
+    text_pov_words = {}
+    words = re.split(RE_WORDS, text.strip().lower())
+    for pov in pov_words:
+        text_pov_words[pov] = []
+    for word in words:
+        word_pov = get_word_pov(word, pov_words)
+        if word_pov is not None:
+            text_pov_words[word_pov].append(word)
+    return text_pov_words
+
+
+def get_text_pov(text, pov_words=None):
+    """Get the point-of-view of a piece of text
+
+    Parameters:
+    ----------
+    text : str
+        a block of english languaget text
+
+    Returns:
+    -------
+    str
+        the point-of-view of the text (first, second, third)
+        returns None if no point-of-view words found
+    """
+    text_pov_words = parse_pov_words(text, pov_words)
+    for pov in POV_WORDS:
+        if len(text_pov_words[pov]) > 0:
+            return pov
+    return None
